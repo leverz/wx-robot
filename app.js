9cdd18dc081d55d963e1b8f222a3fe824eec9d88
zheng.wang <zheng.wang@renren-inc.com>
2016-11-09 20:46:48 +0800

    使用koa-router获取微信token相关参数

--- app.js
+++ app.js
@@ -6,6 +6,35 @@ require("babel-polyfill");
 
 const Koa = require('koa');
 const app = new Koa();
+const router = require("koa-router")();
+
+function getUrlParam (url, name) {
+    const reg = new RegExp(`(^|&)${name}=([^&]*)(&|$)`, "i"),
+        stringStart = url.indexOf("?") + 1;
+    const r = url.substr(stringStart).match(reg);
+    if (r !== null) {
+        const valueIndex = 2;
+
+        return decodeURIComponent(r[valueIndex]);
+    }
+
+    return null;
+};
+
+router.get("/app", function *(next){
+    this.body = "Hello world!";
+    const signature = getUrlParam(this.request.url, "signature");
+    const timestamp = getUrlParam(this.request.url, "timestamp");
+    const nonce = getUrlParam(this.request.url, "nonce");
+    const echostr = getUrlParam(this.request.url, "echostr");
+
+    console.log("signature:", signature);
+    console.log("timestamp:", timestamp);
+    console.log("nonce:", nonce);
+    console.log("echostr:", echostr);
+});
+app.use(router.routes());
+app.use(router.allowedMethods());
 
 app.use(async (ctx, next) => {
     const start = new Date();
