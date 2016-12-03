6ef6cd0bbd59c5712a9d8f519092b1887c89f0f3
zheng.wang <zheng.wang@renren-inc.com>
2016-12-03 21:16:24 +0800

    使用bluebird 以promise的方式调用redis接口

--- redis.js
+++ redis.js
@@ -1,10 +1,13 @@
 const redis = require("redis");
+const Q = require("bluebird");
+Q.promisifyAll(redis.RedisClient.prototype);
+Q.promisifyAll(redis.Multi.prototype);
 
 redis.createClient({
     host: "127.0.0.1",
     port: "6379"
 });
-
+console.log(redis);
 redis.on("error", error => console.log(`error event - ${redis.host} : ${redis.port} - ${error}`))
 
 const redis_get_string = () => {
@@ -12,3 +15,12 @@ const redis_get_string = () => {
         redis.get("key", (error, resp) => console.log(resp))
     })
 }
+
+const redis_get_string_async = () => {
+    const promise = redis.setAsync("key", ["string"])
+    .then(res => redis.getAsync("key"))
+    .then(res => console.log(res))
+
+    return promise;
+}
+redis_get_string();
