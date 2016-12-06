495633e0dca25456765954e1d9e18585bc20c524
zheng.wang <zheng.wang@renren-inc.com>
2016-12-06 20:01:53 +0800

    完善redis操作demo

--- redis.js
+++ redis.js
@@ -3,24 +3,17 @@ const Q = require("bluebird");
 Q.promisifyAll(redis.RedisClient.prototype);
 Q.promisifyAll(redis.Multi.prototype);
 
-redis.createClient({
+const client = redis.createClient({
     host: "127.0.0.1",
     port: "6379"
 });
-console.log(redis);
-redis.on("error", error => console.log(`error event - ${redis.host} : ${redis.port} - ${error}`))
-
-const redis_get_string = () => {
-    redis.set("key", ["string"], (error, response) => {
-        redis.get("key", (error, resp) => console.log(resp))
-    })
-}
+client.on("error", error => console.log(`error event - ${redis.host} : ${redis.port} - ${error}`))
 
 const redis_get_string_async = () => {
-    const promise = redis.setAsync("key", ["string"])
-    .then(res => redis.getAsync("key"))
+    const promise = client.setAsync("key", ["string"])
+    .then(res => client.getAsync("key"))
     .then(res => console.log(res))
 
     return promise;
 }
-redis_get_string();
+redis_get_string_async().then(() => console.log("Hello, Redis"));
