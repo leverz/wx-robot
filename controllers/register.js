9b0d26dddda4acf58d3f752a4d92074626032983
zheng.wang <zheng.wang@renren-inc.com>
2016-12-07 16:29:05 +0800

    统一接口使用情况

--- controllers/register.js
+++ controllers/register.js
@@ -1,4 +1,17 @@
-const client = require("../model/redis");
+const setUserState = require("../model/setUserState")
+
+const registerString = `欢迎进入注册流程：
+
+请根据下列步骤进行注册
+回复菜单项前的字母进行操作
+=================
+
+a 输入您的微信ID
+b 输入您的昵称
+c 查看并确认您输入的微信ID和昵称
+d 退出注册流程
+
+`
 /**
  * 0 - 回复注册，还没有进行后续操作
  * 1 - 回复a，还没有进行后续操作
@@ -11,23 +24,31 @@ const client = require("../model/redis");
  * @param  {Object} rData   用户信息
  * @return {String}         公众号回复内容
  */
-const checkReg = (data, rData) => {
+const checkReg = (data) => {
     if (/注册/.test(data.Content[0])) {
-        client.hmset(data.ToUserName[0], {"status": "0"})
-        return `欢迎进入注册流程：
-
-请根据下列步骤进行注册
-回复菜单项前的字母进行操作
-=================
-
-a 输入您的微信ID
-b 输入您的昵称
-c 查看并确认您输入的微信ID和昵称
-d 退出注册流程
-
-`
+        await setUserState(data.ToUserName[0], "0")
+        return registerString
     } else {
         return "暂不支持未注册用户，回复【注册】进入注册流程"
+    }
+}
+
+const checkWechatId = (data) => {
+    if (data.Content[0] === "a" || data.Content[0] === "A") {
+        setUserState(data.ToUserName[0], "1")
+        return "请输入您的微信ID"
+    }
+
+    return registerString
+}
+
+const register = (data, rData) => {
+    const userId = data.ToUserName[0]
+    switch(rData.status){
+        case "0":
+            return checkWechatId(data)
+        case "1":
+            return
     }
 }
 
