298ee04e570ff8a718b228043f42618763275787
zheng.wang <zheng.wang@renren-inc.com>
2016-12-08 12:26:30 +0800

    封装Service操作

--- weChat/getAccessToken.js
+++ weChat/getAccessToken.js
@@ -1,29 +1,21 @@
 /**
  * Created by Lever on 16/11/10.
  */
-const request = require("request");
-const config = require("./config");
+const Service = require("./weServ")
+const config = require("./config")
 
 module.exports = code => {
-    const reqUrl = "https://api.weixin.qq.com/cgi-bin/token?";
+    const reqUrl = "https://api.weixin.qq.com/cgi-bin/token?"
     const params = {
         appid: config.appId,
         secret: config.appSecret,
         code: code,
         grant_type: "client_credential"
-    };
+    }
     const options = {
         method: "GET",
         url: reqUrl
-    };
+    }
 
-    return new Promise((resolve, reject) => {
-        request(options, (err, res, body) => {
-            if (res) {
-                resolve(JSON.parse(body));
-            } else {
-                reject(err);
-            }
-        });
-    });
+    return new Service(options)
 };
