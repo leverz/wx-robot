8ab9247122533e6437ffde20d41cb1cf9ecf9b65
zheng.wang <zheng.wang@renren-inc.com>
2016-12-08 15:01:22 +0800

    测试回复延迟

--- controllers/feed.js
+++ controllers/feed.js
@@ -3,6 +3,14 @@
  */
 const getUser = require("../model/getUser")
 const register = require("./register")
+
+const wait = time => new Promise((resolve, reject) => {
+    setTimeout(() => {
+        console.log(time)
+        resolve(time)
+    }, time)
+})
+
 const feed = async (ctx, next) => {
     ctx.status = 200
     ctx.type = "application/xml"
@@ -11,7 +19,16 @@ const feed = async (ctx, next) => {
 
     const rData = await getUser(data.FromUserName[0])
 
-    const content = (!!rData && rData.isReady === "true") || register(data, rData)
+    let content = "";
+
+    if (!!rData && rData.isReady === "true") {
+
+    } else {
+        content = register(data, rData)
+    }
+    console.log("begin");
+    await wait(3000);
+    console.log("end");
 
     ctx.body = `<xml>
 <ToUserName><![CDATA[${data.FromUserName}]]></ToUserName>
